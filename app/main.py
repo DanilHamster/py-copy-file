71e44547924dd0703f14a8ecf2009e56135a8311
Hamster <dtaranenko137@gmail.com>
2025-07-02 12:48:09 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    firs_file = parts[1]
+    second_file = parts[2]
+
+    if firs_file == second_file:
+        return
+
+    try:
+        with (open(firs_file, "r") as file_in,
+              open(second_file, "w") as file_out):
+            file_out.write(file_in.read()
+                           )
+
+    except FileNotFoundError:
+        pass
